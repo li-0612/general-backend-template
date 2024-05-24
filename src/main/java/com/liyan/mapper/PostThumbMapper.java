e6d5ad0e91755176e91d1b1dd9f06df43037e10e
李岩 <18121330612@163.com>
2024-05-24 13:57:05 +0800

    init

---
+++ src/main/java/com/liyan/mapper/PostThumbMapper.java
@@ -0,0 +1,15 @@
+package com.liyan.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.liyan.model.entity.PostThumb;
+
+/**
+ * 帖子点赞数据库操作
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
---
+++ src/main/java/com/liyan/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.liyan.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.liyan.model.entity.User;
+
+/**
+ * 用户数据库操作
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
